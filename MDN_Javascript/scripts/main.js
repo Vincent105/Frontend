045eceba3d95210132f09b754bbd41022ef6e5a0
vincent105 <vincent1050917@gmail.com>
2021-11-25 23:37:01 +0800

    1125

--- MDN_Javascript/scripts/main.js
+++ MDN_Javascript/scripts/main.js
@@ -44,3 +44,16 @@ function updateName() {
 
     para.textContent = 'Player2:' + name;
 }
+
+//Test Case 2
+function createParagraph() {
+    let para = document.createElement('p');
+    para.textContent = 'You clicked the button!';
+    document.body.appendChild(para);
+}
+
+const buttons = document.querySelectorAll('button');
+
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', createParagraph);
+}
