ffe3572dd601a0f388b2e47be3b94cb4bc103b03
vincent105 <vincent1050917@gmail.com>
2021-11-26 23:29:36 +0800

    1126

---
+++ MDN_Javascript/scripts/02main_array.js
@@ -0,0 +1,21 @@
+//Create Array 1
+var a = new Array();
+
+a[0] = "apple";
+a[1] = "boy";
+a[2] = "cat";
+
+console.log(a.length);
+
+//Create Array 2
+var b = [];
+
+b[0] = "a";
+b[1] = "b";
+b[2] = "c";
+
+console.log(b.length);
+
+//Create Array 3
+var c = ["a", "b", "c"];
+console.log(c.length);
