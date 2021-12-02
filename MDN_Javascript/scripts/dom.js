8e589a5607121176949eec88c49c8765c0cab4c1
vincent105 <vincent1050917@gmail.com>
2021-12-03 00:09:19 +0800

    120

--- MDN_Javascript/scripts/dom.js
+++ MDN_Javascript/scripts/dom.js
@@ -13,3 +13,34 @@ console.log(e12.previousSibling.textContent);
 
 var e12 = document.querySelector('span');
 console.log(e12.nextSibling.nextSibling.textContent);
+
+var outerdiv = document.getElementById('outer');
+var alldiv = document.getElementsByTagName('div');
+console.log(alldiv.length);
+outerdiv.innerHTML = '';
+console.log(alldiv.length);
+
+//create Div
+let newDiv = document.createElement('div');
+let textNode = document.createTextNode("Hello World!!");
+
+newDiv.id = "test";
+newDiv.className = "box";
+newDiv.appendChild(textNode);
+
+//Create Fragment
+let ul = document.getElementById("mylist");
+let fragment = document.createDocumentFragment();
+
+for (let i = 0; i < 3; i++)
+{
+    let li = document.createElement('li');
+    li.appendChild(document.createTextNode("Item" + (i + 1)));
+    fragment.appendChild(li);
+}
+
+ul.appendChild(fragment);
+
+
+//DOM append
+let mylist = document.getElementById("mylist");
