c97fde2053c672b3f25951d7ace92c7e66113904
vincent105 <vincent1050917@gmail.com>
2023-01-09 01:00:54 +0800

    0109

---
+++ MDN_Javascript2023/scripts/010206Strings.js
@@ -0,0 +1,32 @@
+let string = 'test';
+string;
+
+let badstring = string;
+badstring;
+
+//字串中的跳脫字元（Escaping characters）
+let bigMouth = 'I\' have a dream...';
+bigMouth;
+
+//連接字串（Concatenating strings）
+let one = 'Hello, ';
+let two = 'how are you?';
+let joined = one + two;
+joined;
+
+//型別轉換
+let myString = '123';
+let myNum = Number(myString);
+console.log(typeof (myNum));
+
+let myNum1 = 123;
+let myString1 = myNum1.toString();
+console.log(typeof (myString1));
+
+//模版字符串(Template literals)
+let song = 'Fight the Youth';
+let score = 9;
+let highestScore = 10;
+output = `I like the song "${song}". I gave it a score of ${score / highestScore * 100}%.`;
+
+console.log(output)
