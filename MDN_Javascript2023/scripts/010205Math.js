243775a46266febf25be305b88bcdf8b808a18ea
vincent105 <vincent1050917@gmail.com>
2023-01-09 01:00:16 +0800

    0109

--- MDN_Javascript2023/scripts/010205Math.js
+++ MDN_Javascript2023/scripts/010205Math.js
@@ -9,3 +9,24 @@ console.log(60 % 3);
 let num1 = 10;
 let num2 = 50;
 console.log(9 * num1);
+
+var num3 = 4;
+num3++;
+console.log(num3);
+
+var num4 = 6;
+num4--;
+num4;
+console.log(num4);
+
+/*賦值運算符
+ */
+var x = 3; // x contains the value 3
+var y = 4; // y contains the value 4
+x = y; // x now contains the same value y contains, 4
+
+var x1 = 3; // x contains the value 3
+var y1 = 4; // y contains the value 4
+x1 *= y1; // x now contains the value 12
+
+console.log();
