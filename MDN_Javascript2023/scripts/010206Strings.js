37187b84f24d82f149e9009be2da2d71e4104aca
vincent105 <vincent1050917@gmail.com>
2023-01-09 23:58:33 +0800

    0109

--- MDN_Javascript2023/scripts/010206Strings.js
+++ MDN_Javascript2023/scripts/010206Strings.js
@@ -28,5 +28,12 @@ let song = 'Fight the Youth';
 let score = 9;
 let highestScore = 10;
 output = `I like the song "${song}". I gave it a score of ${score / highestScore * 100}%.`;
+console.log(output)
 
+let examScore = 45;
+let examHighScore = 70;
+examReport = `You scorec ${examScore}/${examHighScore}(${Math.round((examScore / examHighScore) * 100)}%).${examScore >= 49 ? 'Welldone' : 'bad luck'}`;
+console.log(examReport);
+
+output = 'I like the song "' + song + '".\nI gave it a score of ' + (score / highestScore * 100) + '%.';
 console.log(output)
